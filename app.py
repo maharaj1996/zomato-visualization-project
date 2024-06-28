4f5466ca12dff0ffd0944240282c520bd4ce2961
MAHARAJ MUKHERJEE <maharaj.dav2427@gmail.com>
2024-06-28 17:50:26 +0530

    hello

--- app.py
+++ app.py
@@ -16,5 +16,34 @@ def restaurant():
    return render_template("restaurant.html") 
 
 
+
+@app.route('/contact', methods = ["GET","POST"])
+def contactus():
+    if request.method=='POST':
+        name = request.form.get("name")
+        email = request.form.get("email")
+        country = request.form.get("country")
+        state = request.form.get("state")
+        message = request.form.get("message")
+        print(name,email,country,state,message)
+        conn = sqlite3.connect('contactus.db')
+        cur = conn.cursor()
+        cur.execute(f'''
+        INSERT INTO CONTACT VALUES(
+                    "{name}","{email}",
+                    "{country}","{state}",
+                    "{message}"
+        )
+        ''')
+        conn.commit()
+        return render_template('message.html')
+    else:
+        return render_template('contactus.html')
+
+
+
+
+
+
 if __name__ == '__main__' :
     app.run(host="0.0.0.0", port= 5500)
